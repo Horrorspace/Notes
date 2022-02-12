d315f5e9529312788798ae3236094f509cfc7c73
Urkin Eugeniy <utkineugeniy@gmail.com>
2022-02-12 12:41:18 +0700

    prettier fix

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,27 +1,25 @@
-import {NestFactory} from '@nestjs/core';
+import { NestFactory } from '@nestjs/core';
 import {
     FastifyAdapter,
     NestFastifyApplication,
 } from '@nestjs/platform-fastify';
-import {AppModule} from './app.module';
-
+import { AppModule } from './app.module';
 
 async function start() {
     try {
         console.log(`mode is: ${process.env.NODE_ENV}`);
 
         const PORT: number = Number(process.env.PORT) || 3000;
-    
+
         const app = await NestFactory.create<NestFastifyApplication>(
             AppModule,
             new FastifyAdapter({
-                logger: true
-            })
+                logger: true,
+            }),
         );
-        await app.listen(3000, '0.0.0.0');
+        await app.listen(PORT);
         console.log(`HTTP server has been started on port ${PORT}...`);
-    }
-    catch(e) {
+    } catch (e) {
         console.error(e);
     }
 }
