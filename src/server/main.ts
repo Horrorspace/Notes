0c4917eb25f08cedad53ecd4df0f0cc948f55dda
Urkin Eugeniy <utkineugeniy@gmail.com>
2022-02-12 11:44:09 +0700

    add app module and fastify adapter

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,9 +1,29 @@
-export async function start() {
-    console.log(`mode is: ${process.env.NODE_ENV}`);
+import {NestFactory} from '@nestjs/core';
+import {
+    FastifyAdapter,
+    NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import {AppModule} from './app.module';
 
-    const PORT: number = Number(process.env.PORT) || 3000;
 
-    console.log(`HTTP server has been started on port ${PORT}...`);
+async function start() {
+    try {
+        console.log(`mode is: ${process.env.NODE_ENV}`);
+
+        const PORT: number = Number(process.env.PORT) || 3000;
+    
+        const app = await NestFactory.create<NestFastifyApplication>(
+            AppModule,
+            new FastifyAdapter({
+                logger: true
+            })
+        );
+        await app.listen(3000, '0.0.0.0');
+        console.log(`HTTP server has been started on port ${PORT}...`);
+    }
+    catch(e) {
+        console.error(e);
+    }
 }
 
 start();
